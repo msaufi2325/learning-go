09a82cbf1f19a027048aeaa222858ad0954ea4f6
Saufi <msaufi2325@gmail.com>
2024-01-25 01:06:46 +0900

    Refactor main.go to use a slice instead of a struct

--- main.go
+++ main.go
@@ -2,23 +2,13 @@ package main
 
 import "log"
 
-type myStruct struct {
-	FirstName string
-}
-
-func (m *myStruct) printFirstName() string {
-	return m.FirstName
-}
-
 func main() {
-	var myVar myStruct
-	myVar.FirstName = "John"
+	var mySlice []string
 
-	myVar2 := myStruct{
-		FirstName: "Mary",
-	}
+	mySlice = append(mySlice, "Trevor")
+	mySlice = append(mySlice, "John")
+	mySlice = append(mySlice, "Mary")
 
-	log.Println("myVar is set to ", myVar.printFirstName())
-	log.Println("myVar2 is set to ", myVar2.printFirstName())
+	log.Println(mySlice)
 
 }
