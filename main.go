f060e0952fff483b4eaafed40be5355a821dd582
Saufi <msaufi2325@gmail.com>
2024-01-24 22:41:43 +0900

    Refactor function to improve performance

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	var whatToSay string
+	var i int64
+
+	whatToSay = "Goodbye, cruel world!"
+	fmt.Println(whatToSay)
+
+	i = 7
+	fmt.Println("i is set to", i)
+}
