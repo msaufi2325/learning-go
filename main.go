0b42a8ecb06da636337060db53dd08c8a24da3cf
Saufi <msaufi2325@gmail.com>
2024-01-25 00:49:15 +0900

    Refactor printFirstName method to use struct method

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ type myStruct struct {
 	FirstName string
 }
 
+func (m *myStruct) printFirstName() string {
+	return m.FirstName
+}
+
 func main() {
 	var myVar myStruct
 	myVar.FirstName = "John"
@@ -14,7 +18,7 @@ func main() {
 		FirstName: "Mary",
 	}
 
-	log.Println("myVar is set to ", myVar.FirstName)
-	log.Println("myVar2 is set to ", myVar2.FirstName)
+	log.Println("myVar is set to ", myVar.printFirstName())
+	log.Println("myVar2 is set to ", myVar2.printFirstName())
 
 }
